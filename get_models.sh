52a26241ae99e3f19c3f0157a11805f062d92eae
Kurt Manninen <3514241+kmanninen@users.noreply.github.co>
2025-07-24 06:11:39 -0700

    Added anchors and streamlined yolo notebook.

--- get_models.sh
+++ get_models.sh
@@ -9,3 +9,12 @@ wget https://data.brainchip.com/models/AkidaV2/akidanet/akidanet_vww_i8_w4_a4.h5
 
 find . -maxdepth 1 -type f \( -name "*.h5" \) -exec cnn2snn convert -m  {} \;
 rm *.h5
+
+
+# Get datasets for examples
+cd ..
+mkdir -p datasets
+cd datasets
+
+wget https://data.brainchip.com/dataset-mirror/voc/test_20_classes.tfrecord
+wget https://data.brainchip.com/dataset-mirror/coco/coco_anchors.pkl
