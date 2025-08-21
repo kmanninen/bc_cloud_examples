e788fe276eea244ed1e68aa6da2c5cd9c1d8f5f1
Kurt Manninen <kmanninen@brainchip.com>
2025-08-21 14:49:55 -0700

    Added CenterNet Example.

--- get_models.sh
+++ get_models.sh
@@ -6,6 +6,7 @@ wget https://data.brainchip.com/models/AkidaV2/yolo/yolo_akidanet_voc_i8_w8_a8.h
 wget https://data.brainchip.com/models/AkidaV2/tenn_spatiotemporal/tenn_spatiotemporal_eye_buffer_i8_w8_a8.h5
 wget https://data.brainchip.com/models/AkidaV2/tenn_spatiotemporal/tenn_spatiotemporal_jester_buffer_i8_w8_a8.h5
 wget https://data.brainchip.com/models/AkidaV2/akidanet/akidanet_vww_i8_w4_a4.h5
+wget https://data.brainchip.com/models/AkidaV2/centernet/centernet_akidanet18_voc_384_i8_w8_a8.h5
 
 find . -maxdepth 1 -type f \( -name "*.h5" \) -exec cnn2snn convert -m  {} \;
 rm *.h5
